ca3593c615e34200816989e65c93f91c71973662
João Arthur Britto <arthur1081@hotmail.com>
2023-08-28 12:02:59 -0300

    Changes in static configs

--- menu/urls.py
+++ menu/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("/", views.main ,name='menu-principal'),
+    path("", views.main ,name='menu-principal'),
     path("login/", views.login_func, name="login"),
     path("logout/", views.logout_func, name="logout")
 ]
